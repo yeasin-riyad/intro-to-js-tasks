8c44fcc8dfc4b7b7fd828c4eabe3e4b8afcdc647
YeasinRiyad <mazumderyeasin98@gmail.com>
2024-01-26 12:39:12 -0800

    Task 3 done

--- index.js
+++ index.js
@@ -9,3 +9,19 @@
 // alert("The Remaining Balance You have: " + (myMoney-totalPrice));
 
 // Task 2
+
+
+// var math=Number(prompt("Enter The number of Mathematics: "));
+// var Biology=Number(prompt("Enter The number of Biology: "));
+// var Chemistry=Number(prompt("Enter The number of Chemistry: "));
+// var physics=Number(prompt("Enter the number of Physics: "));
+// var Bangla=Number(prompt("Enter the number of Bangla: "));
+// var average=((math+Biology+Chemistry+physics+Bangla)/5) ;
+// alert("The Average marks is "+average);
+
+// Task 3
+
+var number=Number(prompt("Enter a number Please: "));
+var remainder=number%5;
+alert( "The remainder is "+remainder
+);
