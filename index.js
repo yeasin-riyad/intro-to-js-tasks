0eeffcd4ab2cdeba64019a9a3bbb8ef82fe2a8b6
YeasinRiyad <mazumderyeasin98@gmail.com>
2024-01-26 12:07:57 -0800

    first commit

---
+++ index.js
@@ -0,0 +1,11 @@
+
+// Task  1
+
+// var myMoney=prompt("How Much Money Do You Have:");
+// var applePrice=Number (prompt("Enter the Price Of Apple: "));
+// var orangePrice= Number (prompt("Enter The Price of Orange: "));
+// var totalPrice=applePrice+orangePrice;
+// alert('The total price is '+ totalPrice);
+// alert("The Remaining Balance You have: " + (myMoney-totalPrice));
+
+// Task 2
